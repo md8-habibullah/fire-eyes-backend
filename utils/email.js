33d7ec76f290cf417634d5bd27a336f9ee8c2ab9
md8-habibullah <md8.habibullah@gmail.com>
2025-05-27 02:42:40 +0600

    faild re

--- utils/email.js
+++ utils/email.js
@@ -1,5 +1,8 @@
 import nodemailer from 'nodemailer';
 
+console.log('[EMAIL] EMAIL_USER:', process.env.EMAIL_USER);
+console.log('[EMAIL] EMAIL_PASS:', process.env.EMAIL_PASS ? '***HIDDEN***' : 'NOT SET');
+
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   host: 'smtp.gmail.com',
@@ -11,8 +14,17 @@ const transporter = nodemailer.createTransport({
   }
 });
 
+console.log('[EMAIL] Transporter config:', {
+  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
+  user: process.env.EMAIL_USER
+});
+
 export async function sendAlertEmail(to, subject, text, html = null) {
   try {
+    console.log('[EMAIL] Sending email to:', to);
     let info = await transporter.sendMail({
       from: `"Fire Eyes" <${process.env.EMAIL_USER}>`,
       to,
